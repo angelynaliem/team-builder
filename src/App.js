2a7a345428a2009181f2f0a5030a05e6968af1f8
angelynaliem <angelynaliem@gmail.com>
2020-08-07 22:58:25 -0400

    Completed MVP

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
        <h1>Our Members</h1>
        <Form addNewMember={addNewMember} name="Default name" />
       <MembersList members={members} />
