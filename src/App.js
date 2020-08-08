124048de25e25c651f4bce4ce09dc6f364cd9016
angelynaliem <angelynaliem@gmail.com>
2020-08-07 22:19:38 -0400

    Set up state in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOM from "react-dom";
+import data from "./data";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [members, setMembers] = useState([
+    {
+      id: 1,
+      name: "Angelyn Liem",
+      email: "angelyn-liem@lambdastudents.com",
+      role:
+        "Full Stack Web Development"
+    },
+    {
+        id: 2,
+        name: "Asher Jackie Miao",
+        email: "asher-jackie-miao@dinosaurland.com",
+        role:
+          "Boss"
+    },
+    {
+        id: 3,
+        name: "Darrell Liem Miao",
+        email: "darrell-liem-miao@buffetland.com",
+        role:
+          "Asher's partner-in-crime"
+    },
+    {
+        id: 4,
+        name: "Darren Chuan-Roo Miao",
+        email: "darren-chuan-roo-miao@coffeeland.com",
+        role:
+          "Parry Gripp's DJ"
+    }
+  ]);
+
+  const addNewMember = (member) => {
+    setMembers([...members, {...member, id: Date.now() }])
+
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Our Members</h1>
+       {/* // <NoteForm addNewNote={addNewNote} title="Default title" />
+      // <Notes notes={notes} /> */}
       </header>
     </div>
   );
 }
 
 export default App;
+ 
+  
+  
